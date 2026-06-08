c19976f54922760d4934282fef3aee577dff4a3d
dib210 <dibyanshubhr0@gmail.com>
2026-06-08 13:24:58 +0530

    Ass.11

--- optimization_practice/two_sum.py
+++ optimization_practice/two_sum.py
@@ -87,3 +87,27 @@ O(1) time, giving an overall O(n) algorithm.
 =================================================
 
 """
+def two_sum_brute(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return (i, j)
+
+
+def two_sum_fast(nums, target):
+    num_dict = {}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in num_dict:
+            return (num_dict[complement], i)
+
+        num_dict[nums[i]] = i
+
+
+nums = list(map(int, input("Enter numbers separated by spaces: ").split()))
+target = int(input("Enter target value: "))
+
+print("Brute Force:", two_sum_brute(nums, target))
+print("Optimized:  ", two_sum_fast(nums, target))
